44291f179d0435cd1120678671ea65863305fdc5
Subha Sintaveevonge <100717747+Subhavich@users.noreply.github.com>
2024-10-19 20:57:49 +0700

    add Loading Logic to AppPage

--- mal-queries/src/pages/Home.tsx
+++ mal-queries/src/pages/Home.tsx
@@ -2,12 +2,7 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <>
-      <main className="home-main container">
-        <h1>I tried to fetch animes with MAL api</h1>
-        <Link to="/App">
-          <button>Here is the result</button>
-        </Link>
-      </main>
+      <main className=""></main>
     </>
   );
 };
