71093c412e23924046a47b7c1baffdf730b5c30a
Subha Sintaveevonge <100717747+Subhavich@users.noreply.github.com>
2024-10-20 05:13:41 +0700

    implement get random anime button

--- mal-queries/src/http.ts
+++ mal-queries/src/http.ts
@@ -6,3 +6,12 @@ export async function fetchAnime(genreId:number){
     }
     return resData
 }
+
+export async function fetchRandomAnime(){
+    const response = await fetch("https://api.jikan.moe/v4/random/anime")
+    const resData = await response.json()
+    if(!response.ok){
+        throw new Error("unable to fetch random")
+    }
+    return resData
+}
--- mal-queries/src/pages/Home.tsx
+++ mal-queries/src/pages/Home.tsx
@@ -1,44 +1,72 @@
 import AnimeCard from "../components/AnimeCard";
 import { UserContext } from "../store/user-context";
-import { useContext } from "react";
-const mockData = {
-  mal_id: 39024,
-  images: {
-    webp: {
-      image_url: "https://cdn.myanimelist.net/images/anime/1857/98299.webp",
-    },
-  },
-  title: "Wu Dong Qian Kun",
-  synopsis:
-    "The Great Yan Empire exists in a world where respect can only be earned through strength. Within this Great Yan Empire, the four great clans have always stood above the rest. Among them, a particular incident in the Lin Clan resulted in the banishment of a certain individual who went on to start his own family, in hopes of one day being recognized again by the Lin Clan, and rejoining them...\n\nHailing from a banished family of the Great Lin Clan, when Lin Dong was very young, he watched, powerless, as his talented father was easily crushed and crippled by the overwhelming genius of the great Lin Clan, Lin Langtian.\n\nWith a despairing father, a heartbroken grandfather, and a suffering family, ever since that fateful day, Lin Dong has been driven by a deep purpose; to take revenge on the man who had taken everything and more from his family.\n\nArmed with nothing but willpower and determination, join Lin Dong as he unknowingly discovers a destiny greater than he could ever hope to imagine when he stumbles upon a mysterious stone talisman...\n\n(Source: Novel Updates)",
-  genres: [
-    {
-      mal_id: 1,
-      name: "Action",
-      url: "https://myanimelist.net/anime/genre/1/Action",
-    },
-    {
-      mal_id: 2,
-      name: "Adventure",
-      url: "https://myanimelist.net/anime/genre/2/Adventure",
-    },
-    {
-      mal_id: 10,
-      name: "Fantasy",
-      url: "https://myanimelist.net/anime/genre/10/Fantasy",
-    },
-  ],
-  score: 8.0,
-  year: 2024,
-};
+import { useContext, useEffect, useState } from "react";
+import { fetchAnime, fetchRandomAnime } from "../http";
+import { CartoonType } from "../types";
+// const mockData = {
+//   mal_id: 39024,
+//   images: {
+//     webp: {
+//       image_url: "https://cdn.myanimelist.net/images/anime/1857/98299.webp",
+//     },
+//   },
+//   title: "Wu Dong Qian Kun",
+//   synopsis:
+//     "The Great Yan Empire exists in a world where respect can only be earned through strength. Within this Great Yan Empire, the four great clans have always stood above the rest. Among them, a particular incident in the Lin Clan resulted in the banishment of a certain individual who went on to start his own family, in hopes of one day being recognized again by the Lin Clan, and rejoining them...\n\nHailing from a banished family of the Great Lin Clan, when Lin Dong was very young, he watched, powerless, as his talented father was easily crushed and crippled by the overwhelming genius of the great Lin Clan, Lin Langtian.\n\nWith a despairing father, a heartbroken grandfather, and a suffering family, ever since that fateful day, Lin Dong has been driven by a deep purpose; to take revenge on the man who had taken everything and more from his family.\n\nArmed with nothing but willpower and determination, join Lin Dong as he unknowingly discovers a destiny greater than he could ever hope to imagine when he stumbles upon a mysterious stone talisman...\n\n(Source: Novel Updates)",
+//   genres: [
+//     {
+//       mal_id: 1,
+//       name: "Action",
+//       url: "https://myanimelist.net/anime/genre/1/Action",
+//     },
+//     {
+//       mal_id: 2,
+//       name: "Adventure",
+//       url: "https://myanimelist.net/anime/genre/2/Adventure",
+//     },
+//     {
+//       mal_id: 10,
+//       name: "Fantasy",
+//       url: "https://myanimelist.net/anime/genre/10/Fantasy",
+//     },
+//   ],
+//   score: 8.0,
+//   year: 2024,
+// };
 
 const HomePage = () => {
   const userData = useContext(UserContext);
+  const [randomAnime, setRandomAnime] = useState<CartoonType | null>();
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [loaded, setLoaded] = useState<boolean>(false);
+
+  const fetchRandom = async () => {
+    setIsFetching(true);
+    setError(null);
+    try {
+      const resData = await fetchRandomAnime();
+      const anime: CartoonType = resData.data;
+      setRandomAnime(anime);
+    } catch (err) {
+      setError("Cannot Fetch Random Animes");
+      setRandomAnime(null);
+    } finally {
+      setIsFetching(false);
+      setLoaded(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchRandom();
+  }, []);
+
   return (
     <>
       <main className=" max-w-5xl mx-auto  ">
-        <div className="flex ">
-          <AnimeCard cartoon={mockData} />
+        <div className="flex">
+          {loaded && <AnimeCard cartoon={randomAnime} />}
+          <button onClick={fetchRandom}>GET RANDOM ANIME</button>
         </div>
       </main>
     </>
