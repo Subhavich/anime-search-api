87f2208f32f6971423a63ea989c416cfe03e1b9d
Subha Sintaveevonge <100717747+Subhavich@users.noreply.github.com>
2024-10-20 03:56:20 +0700

    add user context

---
+++ mal-queries/src/store/user-context.tsx
@@ -0,0 +1,34 @@
+import { createContext, useState, ReactNode } from "react";
+import { CartoonType } from "../types";
+// Define the type for the context value
+interface UserContextType {
+  savedAnime: CartoonType[];
+  setSavedAnime: React.Dispatch<React.SetStateAction<CartoonType[]>>;
+}
+
+// Create the context with a default value
+export const UserContext = createContext<UserContextType | undefined>(
+  undefined
+);
+
+// Define the props for the provider component
+interface UserContextProviderProps {
+  children: ReactNode;
+}
+
+// Create the provider component
+const UserContextProvider: React.FC<UserContextProviderProps> = ({
+  children,
+}) => {
+  const [savedAnime, setSavedAnime] = useState<CartoonType[]>([]);
+  const userValue = {
+    savedAnime,
+    setSavedAnime,
+  };
+
+  return (
+    <UserContext.Provider value={userValue}>{children}</UserContext.Provider>
+  );
+};
+
+export default UserContextProvider;
