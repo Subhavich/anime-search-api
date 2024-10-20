92a0524602555af9dfa460f134be2f83e2d0f11f
Subha Sintaveevonge <100717747+Subhavich@users.noreply.github.com>
2024-10-21 05:32:31 +0700

    add modal show on home rand anime

--- mal-queries/src/components/AnimeCard.tsx
+++ mal-queries/src/components/AnimeCard.tsx
@@ -5,8 +5,10 @@ import {
   useMotionValue,
   useSpring,
 } from "framer-motion";
-
+import { FiMaximize2 } from "react-icons/fi";
 import { CartoonType } from "../types";
+import { useContext } from "react";
+import { ModalContext } from "../store/modal-context";
 
 // backgroundImage: `url(${images.webp.image_url})`,
 
@@ -15,6 +17,10 @@ const HALF_ROTATION_RANGE = 32.5 / 2;
 
 const TiltCard: FC<{ cartoon: CartoonType }> = ({ cartoon }) => {
   const { images, title, score, synopsis } = cartoon;
+
+  const modalData = useContext(ModalContext)
+  if(!modalData){return}
+  const {setIsOpen,AnimeDetailModal,isOpen} = modalData
 
   const ref = useRef<HTMLDivElement>(null);
 
@@ -70,6 +76,10 @@ const TiltCard: FC<{ cartoon: CartoonType }> = ({ cartoon }) => {
         }}
         className="absolute inset-3 grid place-content-center rounded-xl bg-white shadow-lg"
       >
+        <div className="inset-4 hover:text-white text-5xl opacity-0 hover:opacity-80 hover:cursor-pointer transition-all hover:animate-pulse z-50" onClick={() => setIsOpen(true)}>
+            <FiMaximize2 />
+          </div>
+          <AnimeDetailModal title="clint" isOpen={isOpen} setIsOpen={setIsOpen}/>
         <div className="p-4 absolute inset-0 bg-black rounded-xl z-10 opacity-60 flex flex-col"></div>
       </div>
     </motion.div>
