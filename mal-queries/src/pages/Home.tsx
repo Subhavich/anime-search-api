8307f964a0cb82ab2fdd8f03ddd17f750b4fa0ed
Subha Sintaveevonge <100717747+Subhavich@users.noreply.github.com>
2024-10-22 05:08:21 +0700

    move searchbar out of max-w

--- mal-queries/src/pages/Home.tsx
+++ mal-queries/src/pages/Home.tsx
@@ -48,56 +48,58 @@ const HomePage = () => {
   }, []);
 
   return (
-    <main className="max-w-5xl mx-auto ">
-      <p className="text-center text-3xl my-8">Generate Random Anime!</p>
-      {/* Main Card */}
-      <div className="grid grid-cols-12 mb-8 ">
-        <div className="grid col-span-4 h-96">
-          {isFetching && (
-            <p className="w-72 h-94 my-auto text-center text-2xl font-semibold text-neutral-500 animate-bounce">
-              Loading ...
-            </p>
-          )}
-          {!isFetching && randomAnime && <AnimeCard cartoon={randomAnime} />}
-        </div>
-        <div className="flex flex-col col-span-8 justify-between px-2 ">
-          {isFetching && (
-            <>
-              <p className="w-1/2 rounded-md min-h-6 animate-pulse bg-neutral-800"></p>
-              <p className="w-3/4 rounded-md min-h-6 animate-pulse bg-neutral-800"></p>
-              <p className="w-3/4 rounded-md min-h-40 animate-pulse bg-neutral-800"></p>
-            </>
-          )}
+    <>
+      <main className="max-w-5xl mx-auto ">
+        <p className="text-center text-3xl my-8">Generate Random Anime!</p>
+        {/* Main Card */}
+        <div className="grid grid-cols-12 mb-8 ">
+          <div className="grid col-span-4 h-96">
+            {isFetching && (
+              <p className="w-72 h-94 my-auto text-center text-2xl font-semibold text-neutral-500 animate-bounce">
+                Loading ...
+              </p>
+            )}
+            {!isFetching && randomAnime && <AnimeCard cartoon={randomAnime} />}
+          </div>
+          <div className="flex flex-col col-span-8 justify-between px-2 ">
+            {isFetching && (
+              <>
+                <p className="w-1/2 rounded-md min-h-6 animate-pulse bg-neutral-800"></p>
+                <p className="w-3/4 rounded-md min-h-6 animate-pulse bg-neutral-800"></p>
+                <p className="w-3/4 rounded-md min-h-40 animate-pulse bg-neutral-800"></p>
+              </>
+            )}
 
-          {randomAnime && !isFetching && (
-            <div className="flex flex-col justify-start space-y-6">
-              <p className="text-2xl font-sans font-semibold">
-                {randomAnime.title}
-              </p>
-              <TagSection cartoon={randomAnime} />
-              <p className="line-clamp-4 leading-loose">
-                {randomAnime.synopsis}
-              </p>
+            {randomAnime && !isFetching && (
+              <div className="flex flex-col justify-start space-y-6">
+                <p className="text-2xl font-sans font-semibold">
+                  {randomAnime.title}
+                </p>
+                <TagSection cartoon={randomAnime} />
+                <p className="line-clamp-4 leading-loose">
+                  {randomAnime.synopsis}
+                </p>
+              </div>
+            )}
+            {/* Button Group */}
+            <div className="flex space-x-4">
+              <button
+                className="px-2 py-1 hover:bg-white transition-colors max-w-48 border border-neutral-500 text-lg rounded-lg text-neutral-500"
+                onClick={fetchRandom}
+              >
+                Get Random Anime
+              </button>
+              <button className="px-2 py-1 hover:bg-white transition-colors max-w-48 border border-neutral-500 text-lg rounded-lg text-neutral-500">
+                <Link to={"/app"}>Browse Top Animes</Link>
+              </button>
             </div>
-          )}
-          {/* Button Group */}
-          <div className="flex space-x-4">
-            <button
-              className="px-2 py-1 hover:bg-white transition-colors max-w-48 border border-neutral-500 text-lg rounded-lg text-neutral-500"
-              onClick={fetchRandom}
-            >
-              Get Random Anime
-            </button>
-            <button className="px-2 py-1 hover:bg-white transition-colors max-w-48 border border-neutral-500 text-lg rounded-lg text-neutral-500">
-              <Link to={"/app"}>Browse Top Animes</Link>
-            </button>
           </div>
         </div>
-      </div>
-      <hr className="opacity-80" />
-      {/* Search Bar */}
+        <hr className="opacity-80" />
+        {/* Search Bar */}
+      </main>
       <SearchBar />
-    </main>
+    </>
   );
 };
 
