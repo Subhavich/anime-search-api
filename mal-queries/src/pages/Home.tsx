5cfd66ff2bb9687d65817d427504c216abbce7e0
Subha Sintaveevonge <100717747+Subhavich@users.noreply.github.com>
2024-10-23 03:21:51 +0700

    fix button text-size on main

--- mal-queries/src/pages/Home.tsx
+++ mal-queries/src/pages/Home.tsx
@@ -84,15 +84,15 @@ const HomePage = () => {
             {/* Button Group */}
             <div className="flex flex-col items-center pt-8 sm:pt-0 space-y-8 sm:space-y-0 sm:flex-row sm:space-x-4 sm:text-xl">
               <button
-                className="  px-2 py-1 hover:bg-white transition-colors max-w-48 border border-neutral-500 text-lg rounded-lg text-neutral-500"
+                className="  px-2 py-1 hover:bg-white transition-colors max-w-48 border border-neutral-500 text-base rounded-lg text-neutral-500"
                 onClick={fetchRandom}
               >
                 Get Random Anime
               </button>
-              <button className="  px-2 py-1 hover:bg-white transition-colors max-w-48 border border-neutral-500 text-lg rounded-lg text-neutral-500">
+              <button className="  px-2 py-1 hover:bg-white transition-colors max-w-48 border border-neutral-500 text-base rounded-lg text-neutral-500">
                 <Link to={"/app"}>Browse Top Animes</Link>
               </button>
-              <button className="  px-2 py-1 hover:bg-white transition-colors max-w-48 border border-neutral-500 text-lg rounded-lg text-neutral-500">
+              <button className="  px-2 py-1 hover:bg-white transition-colors max-w-48 border border-neutral-500 text-base rounded-lg text-neutral-500">
                 <a href="#search">Advanced Search</a>
               </button>
             </div>
